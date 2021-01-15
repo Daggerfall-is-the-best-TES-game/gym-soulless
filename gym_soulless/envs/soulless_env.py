9f7bd19ab34522948474a2fc2aa3da9a8bee9689
Daggerfall-is-the-Best-TES-Game <Davidcfried@gmail.com>
2021-01-15 01:04:43 -0500

    added error code for window deathcount

--- gym_soulless/envs/soulless_env.py
+++ gym_soulless/envs/soulless_env.py
@@ -17,6 +17,7 @@ class SoullessEnv(gym.Env):
     DEATHCOUNT_PATTERN = re.compile("(\d+)")
     KEYS = ("VK_LEFT", "VK_RIGHT", "VK_LSHIFT")
     TRANSITION_TO_ACTION = {"10": "up", "01": "down", "00": False, "11": False}
+
     def __init__(self):
         self.application = self.start_game()
         self.process_id = self.application.process
@@ -25,16 +26,16 @@ class SoullessEnv(gym.Env):
         self.navigate_main_menu()
         self.enter_avoidance()
         self.process.suspend()
-        
+
         self.PREVIOUS_ACTION = 0
         self.deathcount = self.get_deathcount()
         self.action_space = spaces.Discrete(6)
-        self.observation_space = spaces.Box(low=0, high=255, shape=self.get_observation_space_size(), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=255, shape=self.get_observation_space_size())
 
     def start_game(self):
         """starts the Soulless process"""
-        app = Application().start("Soulless 1.3HM/Soulless Hard Mode.exe")
-        #app = Application().connect(title_re="Soulless.*", timeout=20)
+        app = Application().start("D:/Users/david/PycharmProjects/reinforcement-learning/gym-soulless/Soulless 1.3HM/Soulless Hard Mode.exe")
+        # app = Application().connect(title_re="Soulless.*", timeout=20)
         return app
 
     def get_window_dialog(self):
@@ -58,14 +59,18 @@ class SoullessEnv(gym.Env):
         """:returns a np array image of the dialog cropped to exclude the margins for resizing the window"""
         rect = self.dialog.rectangle()
         left, top, right, bottom = rect.left + 8, rect.top, rect.right - 8, rect.bottom - 7
-        return np.array(grab(bbox=(left, top, right, bottom)), dtype=np.uint8)
+        return np.array(grab(bbox=(left, top, right, bottom)))
 
     def step(self, action: int):
         """expects the game to be in a suspended state"""
         self.dialog.set_focus()
-        current_deathcount = self.get_deathcount()
-        is_done = current_deathcount > self.deathcount
-        self.deathcount = current_deathcount
+        try:
+            current_deathcount = self.get_deathcount()
+            is_done = current_deathcount > self.deathcount
+            self.deathcount = current_deathcount
+        except TypeError as e:
+            print("could not find deathcount")
+            is_done = False
         obs = self.capture_window()
         keystrokes = self.get_action_transition(self.PREVIOUS_ACTION, action)
         self.perform_action(keystrokes)
@@ -105,7 +110,7 @@ class SoullessEnv(gym.Env):
         self.application.kill()
 
     def get_observation_space_size(self):
-        return self.capture_window().shape
+        return tuple([*self.capture_window().shape, 3])
 
     def get_deathcount(self):
         """:returns the number of times the kid has died"""
